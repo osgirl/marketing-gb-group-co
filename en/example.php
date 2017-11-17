8e1cbf914a69d8c64927772a0de246eed13d7a7a
Yoann Grange <yoann.grange@gmail.com>
2017-11-17 10:04:21 +0100

    Test

---
+++ en/example.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'script/YouTubeDownloader.php';
+include_once 'script/LinkHandler.php';
+
+$url = "https://www.youtube.com/watch?v=oeCihv9A3ac";
+$handler = new LinkHandler();
+$downloader = $handler->getDownloader($url);
+$downloader->setUrl($url);
+if($downloader->hasVideo())
+{
+    print_r($downloader->getVideoDownloadLink());
+}
