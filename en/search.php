9c06b670b05f5f401aeaa357a6f509d3bbe06382
Yoann Grange <yoann.grange@gmail.com>
2017-11-28 22:55:35 +0100

    Algolia test

--- en/search.php
+++ en/search.php
@@ -1,3 +1,20 @@
 <?php 
 require_once('../vendor/algolia/algoliasearch.php');
+
+$client = new \AlgoliaSearch\Client('BG3AX4OCRB', '0a5e0edb3588b17ea4255c35f077e00d');
+
+$index = $client->initIndex('records');
+
+$index->setSettings(array(
+  "searchableAttributes" => [
+    "brand",
+    "name",
+    "categories",
+    "unordered(description)"
+  ],
+  "customRanking" => [
+    "desc(popularity)"
+  ]
+));
+
 ?>
