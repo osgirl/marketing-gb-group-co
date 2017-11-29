2f3f13a55e4eeabd7931aa2e80123155b2210b77
Yoann Grange <yoann.grange@gmail.com>
2017-11-29 12:02:40 +0100

    Forgot the php

--- en/search.php
+++ en/search.php
@@ -3,17 +3,15 @@ require_once('../vendor/algolia/algoliasearch.php');
 
 $client = new \AlgoliaSearch\Client('BG3AX4OCRB', '0a5e0edb3588b17ea4255c35f077e00d');
 
-$index = $client->initIndex('records');
+$index = $client->initIndex('catalogues');
 
 $index->setSettings(array(
   "searchableAttributes" => [
-    "brand",
-    "name",
-    "categories",
-    "unordered(description)"
+    "serie",
+    "model"
   ],
   "customRanking" => [
-    "desc(popularity)"
+    "asc(serie)"
   ]
 ));
 ?>
