4465194896c176dfc83d616158da2120fd7aef54
Yoann Grange <yoann.grange@gmail.com>
2017-11-29 11:59:16 +0100

    Test with catalogues

--- en/app.js
+++ en/app.js
@@ -2,7 +2,7 @@ var search = instantsearch({
   // Replace with your own values
   appId: 'BG3AX4OCRB',
   apiKey: '0a5e0edb3588b17ea4255c35f077e00d', // search only API key, no ADMIN key
-  indexName: 'records',
+  indexName: 'catalogues',
   urlSync: true
 });
 
