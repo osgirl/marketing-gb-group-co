d8a6b8cc74fbac8d42b34ec2b4b3dcefc112f2af
Yoann Grange <yoann.grange@gmail.com>
2017-11-29 18:55:27 +0100

    Test

--- en/app.js
+++ en/app.js
@@ -17,7 +17,7 @@ search.addWidget(
     container: '#hits',
     hitsPerPage: 8,
     templates: {
-      item: "<div class=\"col-6 col-sm-3 col-md-3\"><a href=\"http://marketing.cellar.services.clever-cloud.com/catalogues/gregoire-besson/{{filename}}\" download><div class=\"card\"><div class=\"card-body\"><img class=\"img-fluid\" src=\"../img/catalogues/catalogue.png\" alt=\"Gregoire Besson Parts Catalogue {{serie}} - {{model}}\"><br><h6>Serial: {{serie}}</h6><p><small>Model: {{model}}</small></p></div></div></a></div>",
+      item: document.getElementById('hit-template').innerHTML,
       empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
     }
   })
