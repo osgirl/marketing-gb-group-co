c61df8f6679468fa199851824d3236e2d0819e32
Yoann Grange <yoann.grange@gmail.com>
2017-11-29 23:37:08 +0100

    Clean

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('../vendor/algolia/algoliasearch.php');
+require_once('vendor/algolia/algoliasearch.php');
 
 $client = new \AlgoliaSearch\Client('BG3AX4OCRB', '0a5e0edb3588b17ea4255c35f077e00d');
 
