437322f9dd39f1ae8d9a00a272c7a8fbe42e60ac
Yoann Grange <yoann.grange@gmail.com>
2018-01-04 16:53:11 +0100

    Adding ppt template, graphic charter and normandie photos

---
+++ photos/normandie/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Normandie Photos - GB Group Marketing Tools &amp; Brand Assets - Grégoire Besson, Rabe, Agriway</title>
+    <meta name="description" content="">
+    <meta name="author" content="GB Group">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext|Kanit:500" rel="stylesheet">
+    <link href="../css/instantsearch.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="manifest" href="../manifest.json">
+    <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#3b3b3c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108104860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108104860-7');
+    </script>
+
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Normandie</h1>
+                <div class="row">
+                    <?php
+                    $arr = array(
+                    "normandie-pf-00001.jpg",
+                    "normandie-pf-00002.jpg",
+                    "normandie-pf-00003.jpg",
+                    "normandie-pr-00001.jpg",
+                    "normandie-pr-00002.jpg",
+                    "normandie-pr-00003.jpg",
+                    "normandie-pr-00004.jpg",
+                    "normandie-pr-00005.jpg",
+                    "normandie-pr-00006.jpg",
+                    "normandie-pr-00007.jpg",
+                    "normandie-pr-00008.jpg",
+                    "normandie-pr-00009.jpg",
+                    "normandie-pr-00010.jpg",
+                    "normandie-pr-00011.jpg",
+                    "normandie-pr-00012.jpg",
+                    "normandie-pr-00013.jpg",
+                    "normandie-pr-00014.jpg",
+                    "normandie-pr-00015.jpg",
+                    "normandie-pr-00016.jpg",
+                    "normandie-pr-00017.jpg",
+                    "normandie-pr-00018.jpg",
+                    "normandie-pr-00019.jpg",
+                    "normandie-pr-00020.jpg",
+                    "normandie-pr-00021.jpg",
+                    "normandie-pr-00022.jpg",
+                    "normandie-pr-00023.jpg",
+                    "normandie-pr-00024.jpg",
+                    "normandie-pr-00025.jpg",
+                    "normandie-pr-sp2-sp3-00001.jpg",
+                    "normandie-pr-sp2-sp3-00002.jpg",
+                    "normandie-pr-sp2-sp3-00003.jpg",
+                    "normandie-pr-sp2-sp3-00004.jpg",
+                    "normandie-sp2-00001.jpg",
+                    "normandie-sp2-00002.jpg",
+                    "normandie-sp2-00003.jpg",
+                    "normandie-sp2-00004.jpg",
+                    "normandie-sp2-00005.jpg",
+                    "normandie-sp2-00006.jpg",
+                    "normandie-sp2-00007.jpg",
+                    "normandie-sp2-00008.jpg",
+                    "normandie-sp2-00009.jpg",
+                    "normandie-sp2-00010.jpg",
+                    "normandie-sp2-00011.jpg",
+                    "normandie-sp2-00012.jpg",
+                    "normandie-sp2-00013.jpg",
+                    "normandie-sp2-00014.jpg",
+                    "normandie-sp2-00015.jpg",
+                    "normandie-sp2-00016.jpg",
+                    "normandie-sp2-00017.jpg",
+                    "normandie-sp2-00018.jpg",
+                    "normandie-sp2-00019.jpg",
+                    "normandie-sp2-00020.jpg",
+                    "normandie-sp2-00021.jpg",
+                    "normandie-sp2-00022.jpg",
+                    "normandie-sp2-00023.jpg",
+                    "normandie-sp2-00024.jpg",
+                    "normandie-sp2-00025.jpg",
+                    "normandie-sp2-00026.jpg",
+                    "normandie-sp2-00027.jpg",
+                    "normandie-sp2-00028.jpg",
+                    "normandie-sp2-00029.jpg",
+                    "normandie-sp2-00030.jpg",
+                    "normandie-sp2-00031.jpg",
+                    "normandie-sp2-00032.jpg",
+                    "normandie-sp2-00033.jpg",
+                    "normandie-sp2-00034.jpg",
+                    "normandie-sp2-00035.jpg",
+                    "normandie-sp3-00001.jpg",
+                    "normandie-sp3-00002.jpg",
+                    "normandie-sp3-00003.jpg",
+                    "normandie-sp3-00004.jpg",
+                    "normandie-sp3-00005.jpg",
+                    "normandie-sp3-00006.jpg",
+                    "normandie-sp3-00007.jpg",
+                    "normandie-sp3-00008.jpg",
+                    "normandie-sp3-00009.jpg",
+                    "normandie-sp3-00010.jpg",
+                    "normandie-sp3-00011.jpg",
+                    "normandie-sp3-00012.jpg",
+                    "normandie-sp3-00013.jpg",
+                    "normandie-sp3-00014.jpg",
+                    "normandie-sp3-00015.jpg",
+                    "normandie-sp3-00016.jpg",
+                    "normandie-sp3-00017.jpg",
+                    "normandie-sp3-00018.jpg",
+                    "normandie-sp3-00019.jpg",
+                    "normandie-sp3-00020.jpg",
+                    "normandie-sp3-00021.jpg",
+                    "normandie-sp3-00022.jpg",
+                    "normandie-sp3-00023.jpg",
+                    "normandie-sp3-00024.jpg",
+                    "normandie-sp3-00025.jpg",
+                    "normandie-sp3-00026.jpg",
+                    "normandie-sp3-00027.jpg",
+                    "normandie-sp3-00028.jpg",
+                    "normandie-sp3-00029.jpg",
+                    "normandie-sp3-00030.jpg",
+                    "normandie-sp3-00031.jpg",
+                    "normandie-sp3-00032.jpg",
+                    "normandie-sp4-00001.jpg",
+                    "normandie-sp4-00002.jpg",
+                    "normandie-sp4-00003.jpg",
+                    "normandie-sp4-00004.jpg",
+                    "normandie-sp4-00005.jpg",
+                    "normandie-sp4-00006.jpg",
+                    "normandie-sp4-00007.jpg",
+                    "normandie-sp4-00008.jpg",
+                    "normandie-sp4-00009.jpg",
+                    "normandie-sp4-00010.jpg",
+                    "normandie-sp4-00011.jpg",
+                    "normandie-sp4-00012.jpg",
+                    "normandie-sp4-00013.jpg",
+                    "normandie-sp4-00014.jpg",
+                    "normandie-sp4-00015.jpg",
+                    "normandie-sp4-00016.jpg",
+                    "normandie-sp4-00017.jpg",
+                    "normandie-sp4-00018.jpg",
+                    "normandie-sp4-00019.jpg",
+                    "normandie-sp4-00020.jpg",
+                    "normandie-sp4-00021.jpg"
+                    );
+                    foreach ($arr as $value) {
+                        echo "
+                        <div class=\"col-3\">\n
+                            <img class=\"img-fluid\" src=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/small/".$value."\">\n
+                            <br />\n
+                            <a href=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value."\"><small>Download &darr;</small></a>\n
+                        </div>\n
+                        \n
+                        ";}
+                    ?>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+  </body>
+</html>
