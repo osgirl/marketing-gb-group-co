66c15b31b4d91be07910fb7dc5d7d479284de60d
Yoann Grange <yoann.grange@gmail.com>
2018-01-05 10:57:53 +0100

    Display exif

--- photos/normandie/index.php
+++ photos/normandie/index.php
@@ -163,6 +163,10 @@
                             <div class=\"card\" style=\"margin-bottom: 2rem;\">\n
                                 <div class=\"ratio\" style=\"background-image: url(https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/small/".$value.");\"></div>\n
                                 <a class=\"text-center\" href=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value."\" download target=\"_blank\"><small>Download &darr;</small></a>\n
+                                <p class=\"\">";
+                                $exif = exif_read_data('https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value.', 'IFD0');
+                                echo $exif===false ? "None.<br />\n" : $exif."<br />\n";
+                                echo "</p>\n
                             </div>\n
                         </div>\n
                         \n
