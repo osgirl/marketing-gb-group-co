e00351bce6f12d392aec14bd58b35630bfeca516
Yoann Grange <yoann.grange@gmail.com>
2018-04-17 20:23:30 +0200

    Adding voyager photos

---
+++ photos/voyager/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Voyager Photos - GB Group Marketing Tools &amp; Brand Assets - Grégoire Besson, Rabe, Agriway</title>
+    <meta name="description" content="">
+    <meta name="author" content="GB Group">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext|Kanit:500" rel="stylesheet">
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108104860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108104860-7');
+    </script>
+
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://marketing.gb-group.co">Back to homepage</a>
+                <br>
+                <br>
+                <h1>Voyager</h1>
+                <br>
+                <div class="row">
+                    <?php
+                    $arr = array(
+                    "voyager-2016-00001.jpg",
+                    "voyager-2016-00002.jpg",
+                    "voyager-2016-00003.jpg",
+                    "voyager-2016-00004.jpg",
+                    "voyager-2016-00005.jpg",
+                    "voyager-2016-00006.jpg",
+                    "voyager-2016-00007.jpg",
+                    "voyager-2016-00008.jpg",
+                    "voyager-2016-00009.jpg",
+                    "voyager-2016-00010.jpg",
+                    "voyager-2016-00011.jpg",
+                    "voyager-2016-00012.jpg",
+                    "voyager-2016-00013.jpg",
+                    "voyager-2016-00014.jpg",
+                    "voyager-2016-00015.jpg",
+                    "voyager-2016-00016.jpg",
+                    "voyager-2016-00017.jpg",
+                    "voyager-2016-00018.jpg",
+                    "voyager-2016-00019.jpg",
+                    "voyager-2016-00020.jpg",
+                    "voyager-2016-00021.jpg",
+                    "voyager-2016-00022.jpg",
+                    "voyager-2016-00023.jpg",
+                    "voyager-2016-00024.jpg",
+                    "voyager-2016-00025.jpg",
+                    "voyager-2016-00026.jpg",
+                    "voyager-2016-00027.jpg",
+                    "voyager-2016-00028.jpg",
+                    "voyager-2016-00029.jpg",
+                    "voyager-2016-00030.jpg",
+                    "voyager-2016-00031.jpg",
+                    "voyager-2016-00032.jpg",
+                    "voyager-2016-00033.jpg",
+                    "voyager-2016-00034.jpg",
+                    "voyager-2016-00035.jpg",
+                    "voyager-2016-00036.jpg",
+                    "voyager-2016-00037.jpg",
+                    "voyager-2016-00038.jpg",
+                    "voyager-2016-00039.jpg",
+                    "voyager-2016-00040.jpg",
+                    "voyager-2016-00041.jpg",
+                    "voyager-2016-00042.jpg",
+                    "voyager-2016-00043.jpg",
+                    "voyager-2016-00044.jpg",
+                    "voyager-2016-00045.jpg",
+                    "voyager-2016-00046.jpg",
+                    "voyager-2016-00047.jpg",
+                    "voyager-2016-00048.jpg",
+                    "voyager-2016-00049.jpg",
+                    "voyager-2016-00050.jpg",
+                    "voyager-2016-00051.jpg",
+                    "voyager-2016-00052.jpg",
+                    "voyager-2016-00053.jpg",
+                    "voyager-2016-00054.jpg",
+                    "voyager-2016-00055.jpg",
+                    "voyager-2016-00056.jpg",
+                    "voyager-2016-00057.jpg",
+                    "voyager-2016-00058.jpg",
+                    "voyager-2016-00059.jpg"
+                    );
+                    foreach ($arr as $value) {
+                        echo "
+                        <div class=\"col-3\">\n
+                            <div class=\"card\" style=\"margin-bottom: 2rem;\">\n
+                                <div class=\"ratio\" style=\"background-image: url(https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/small/".$value.");\"></div>\n
+                                <p><small>".$value."</small></p>
+                                <a class=\"text-center\" href=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value."\" download target=\"_blank\"><small>Download &darr;</small></a>\n
+                            </div>\n
+                        </div>\n
+                        \n
+                        ";}
+                    ?>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+  </body>
+</html>
