0bfe78b87515a9a2f74fbd86bf90f0da29139395
Yoann Grange <yoann.grange@gmail.com>
2018-06-25 17:14:56 +0200

    adding cover photos

---
+++ photos/gregoire-besson/cover/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Cover Photos - GB Group Marketing Tools &amp; Brand Assets - Grégoire Besson, Rabe, Agriway</title>
+    <meta name="description" content="">
+    <meta name="author" content="GB Group">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext|Kanit:500" rel="stylesheet">
+    <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108104860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108104860-7');
+    </script>
+
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://marketing.gb-group.co">Back to homepage</a>
+                <br>
+                <br>
+                <h1>Cover</h1>
+                <br>
+                <div class="row">
+                    <?php
+                    $arr = array(
+                    "cover-00001.jpg",
+                    "cover-00002.jpg",
+                    "cover-00003.jpg",
+                    "cover-00004.jpg",
+                    "cover-00005.jpg",
+                    "cover-00006.jpg",
+                    "cover-00007.jpg",
+                    "cover-00008.jpg",
+                    "cover-00009.jpg",
+                    "cover-00010.jpg",
+                    "cover-00011.jpg",
+                    "cover-00012.jpg",
+                    "cover-00013.jpg",
+                    "cover-00014.jpg",
+                    "cover-00015.jpg",
+                    "cover-00016.jpg",
+                    "cover-00017.jpg",
+                    "cover-00018.jpg",
+                    "cover-00019.jpg",
+                    "cover-00020.jpg",
+                    "cover-00021.jpg",
+                    "cover-00022.jpg",
+                    "cover-00023.jpg",
+                    "cover-00024.jpg",
+                    "cover-00025.jpg",
+                    "cover-00026.jpg",
+                    "cover-00027.jpg",
+                    "cover-00028.jpg",
+                    "cover-00029.jpg",
+                    "cover-00030.jpg",
+                    "cover-00031.jpg",
+                    "cover-00032.jpg",
+                    "cover-00033.jpg",
+                    "cover-00034.jpg",
+                    "cover-00035.jpg",
+                    "cover-00036.jpg",
+                    "cover-00037.jpg",
+                    "cover-00038.jpg",
+                    "cover-00039.jpg",
+                    "cover-00040.jpg",
+                    "cover-00041.jpg",
+                    "cover-00042.jpg",
+                    "cover-00043.jpg",
+                    "cover-00044.jpg",
+                    "cover-00045.jpg",
+                    "cover-00046.jpg",
+                    "cover-00047.jpg",
+                    "cover-00048.jpg",
+                    "cover-00049.jpg",
+                    "cover-00050.jpg",
+                    "cover-00051.jpg",
+                    "cover-00052.jpg",
+                    "cover-00053.jpg",
+                    "cover-00054.jpg",
+                    "cover-00055.jpg",
+                    "cover-00056.jpg",
+                    "cover-00057.jpg",
+                    "cover-00058.jpg",
+                    "cover-00059.jpg",
+                    "cover-00060.jpg",
+                    "cover-00061.jpg",
+                    "cover-00062.jpg",
+                    "cover-00063.jpg",
+                    "cover-00064.jpg",
+                    "cover-00065.JPG",
+                    "cover-00066.JPG",
+                    "cover-00067.jpg",
+                    "cover-00068.jpg",
+                    "cover-00069.jpg",
+                    "cover-00070.jpg",
+                    "cover-00071.jpg",
+                    "cover-00072.jpg",
+                    "cover-00073.jpg",
+                    "cover-00074.jpg",
+                    "cover-00075.jpg",
+                    "cover-00076.jpg",
+                    "cover-00077.jpg",
+                    "cover-00078.jpg",
+                    "cover-00079.jpg",
+                    "cover-00080.jpg",
+                    "cover-00081.jpg",
+                    "cover-00082.jpg",
+                    "cover-00083.jpg",
+                    "cover-00084.jpg",
+                    "cover-00085.jpg",
+                    "cover-00086.jpg",
+                    "cover-00087.jpg",
+                    "cover-00088.jpg",
+                    "cover-00089.jpg",
+                    "cover-00090.jpg",
+                    "cover-00091.jpg",
+                    "cover-00092.jpg",
+                    "cover-00093.jpg",
+                    "cover-00094.jpg",
+                    "cover-00095.jpg",
+                    "cover-00096.jpg",
+                    "cover-00097.jpg",
+                    "cover-00098.jpg",
+                    "cover-00099.jpg",
+                    "cover-00100.jpg",
+                    "cover-00101.jpg",
+                    "cover-00102.jpg",
+                    "cover-00103.jpg",
+                    "cover-00104.jpg",
+                    "cover-00105.jpg",
+                    "cover-00106.jpg",
+                    "cover-00107.jpg",
+                    "cover-00108.jpg",
+                    "cover-00109.jpg",
+                    "cover-00110.jpg",
+                    "cover-00111.jpg",
+                    "cover-00112.jpg",
+                    "cover-00113.jpg",
+                    "cover-00114.jpg",
+                    "cover-00115.jpg",
+                    "cover-00116.jpg",
+                    "cover-00117.jpg",
+                    "cover-00118.jpg",
+                    "cover-00119.jpg",
+                    "cover-00120.jpg",
+                    "cover-00121.jpg",
+                    "cover-00122.jpg",
+                    "cover-00123.jpg",
+                    "cover-00124.jpg",
+                    "cover-00125.jpg",
+                    "cover-00126.jpg",
+                    "cover-00127.jpg",
+                    "cover-00128.jpg",
+                    "cover-00129.jpg",
+                    "cover-00130.jpg",
+                    "cover-00131.jpg",
+                    "cover-00132.jpg",
+                    "cover-00133.jpg",
+                    "cover-00134.jpg",
+                    "cover-00135.jpg",
+                    "cover-00136.jpg",
+                    "cover-00137.jpg",
+                    "cover-00138.jpg",
+                    "cover-00139.jpg",
+                    "cover-00140.jpg",
+                    "cover-00141.jpg",
+                    "cover-00142.jpg",
+                    "cover-00143.jpg",
+                    "cover-00144.jpg",
+                    "cover-00145.jpg",
+                    "cover-00146.jpg",
+                    "cover-00147.jpg",
+                    "cover-00148.jpg",
+                    "cover-00149.jpg",
+                    "cover-00150.jpg",
+                    "cover-00151.jpg",
+                    "cover-00152.jpg",
+                    "cover-00153.jpg",
+                    "cover-00154.jpg",
+                    "cover-00155.jpg",
+                    "cover-00156.jpg",
+                    "cover-00157.jpg",
+                    "cover-00158.jpg",
+                    "cover-00159.jpg",
+                    "cover-00160.jpg",
+                    "cover-00161.jpg",
+                    "cover-00162.jpg",
+                    "cover-00163.jpg",
+                    "cover-00164.jpg",
+                    "cover-00165.jpg",
+                    "cover-00166.jpg",
+                    "cover-00167.jpg",
+                    "cover-00168.jpg",
+                    "cover-00169.jpg",
+                    "cover-00170.jpg",
+                    "cover-00171.jpg",
+                    "cover-00172.jpg",
+                    "cover-00173.jpg",
+                    "cover-00174.jpg",
+                    "cover-00175.jpg",
+                    "cover-00176.jpg",
+                    "cover-00177.jpg",
+                    "cover-00178.jpg",
+                    "cover-00179.jpg",
+                    "cover-00180.jpg",
+                    "cover-00181.jpg",
+                    "cover-00182.jpg",
+                    "cover-00183.jpg",
+                    "cover-00184.jpg",
+                    "cover-00185.jpg",
+                    "cover-00186.jpg",
+                    "cover-00187.jpg",
+                    "cover-00188.jpg",
+                    "cover-00189.jpg",
+                    "cover-00190.jpg",
+                    "cover-00191.jpg",
+                    "cover-00192.jpg",
+                    "cover-00193.jpg",
+                    "cover-00194.jpg",
+                    "cover-00195.jpg",
+                    "cover-00196.jpg",
+                    "cover-00197.jpg",
+                    "cover-00198.jpg",
+                    "cover-00199.jpg",
+                    "cover-00200.jpg",
+                    "cover-00201.jpg",
+                    "cover-00202.jpg",
+                    "cover-00203.jpg",
+                    "cover-00204.jpg",
+                    "cover-00205.jpg",
+                    "cover-00206.jpg"
+                    );
+                    foreach ($arr as $value) {
+                        echo "
+                        <div class=\"col-3\">\n
+                            <div class=\"card\" style=\"margin-bottom: 2rem;\">\n
+                                <div class=\"ratio\" style=\"background-image: url(https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/small/".$value.");\"></div>\n
+                                <p class=\"text-center\" style=\"margin: 0; padding: 0.5rem; font-size:0.7rem;\"><small>".$value."</small></p>
+                                <a class=\"text-center\" href=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value."\" download target=\"_blank\"><small style=\"font-weight: bold; padding-bottom: 0.5rem; display: block;\">Download &darr;</small></a>\n
+                            </div>\n
+                        </div>\n
+                        \n
+                        ";}
+                    ?>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+  </body>
+</html>
