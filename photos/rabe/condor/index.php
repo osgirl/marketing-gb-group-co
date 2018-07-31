f7bc47fb6cbc01c3a2029a3e32d3ebd2bbbf2c0f
Yoann Grange <yoann.grange@gmail.com>
2018-07-31 10:45:27 +0200

    adding condor photos

---
+++ photos/rabe/condor/index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Condor Photos - GB Group Marketing Tools &amp; Brand Assets - Grégoire Besson, Rabe, Agriway</title>
+    <meta name="description" content="">
+    <meta name="author" content="GB Group">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext|Kanit:500" rel="stylesheet">
+    <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108104860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108104860-7');
+    </script>
+
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://marketing.gb-group.co">Back to homepage</a>
+                <br>
+                <br>
+                <h1>Condor</h1>
+                <br>
+                <div class="row">
+                    <?php
+                    $arr = array(
+                    "condor-00001.jpg",
+                    "condor-00002.jpg",
+                    "condor-00003.jpg",
+                    "condor-00004.jpg",
+                    "condor-00005.jpg",
+                    "condor-00006.jpg",
+                    "condor-00007.jpg",
+                    "condor-00008.jpg",
+                    "condor-00009.jpg",
+                    "condor-00010.jpg",
+                    "condor-00011.jpg",
+                    "condor-00012.jpg",
+                    "condor-00013.jpg",
+                    "condor-00014.jpg",
+                    "condor-00015.jpg",
+                    "condor-00016.jpg",
+                    "condor-00017.jpg",
+                    "condor-00018.jpg",
+                    "condor-00019.jpg",
+                    "condor-00020.jpg",
+                    "condor-00021.jpg",
+                    "condor-00022.jpg",
+                    "condor-00023.jpg",
+                    "condor-00024.jpg",
+                    "condor-00025.jpg",
+                    "condor-00026.jpg",
+                    "condor-00027.jpg",
+                    "condor-00028.jpg",
+                    "condor-00029.jpg",
+                    "condor-00030.jpg",
+                    "condor-00031.jpg",
+                    "condor-00032.jpg",
+                    "condor-00033.jpg",
+                    "condor-00034.jpg",
+                    "condor-00035.jpg",
+                    "condor-00036.jpg",
+                    "condor-00037.jpg",
+                    "condor-00038.jpg",
+                    "condor-00039.jpg",
+                    "condor-00040.jpg",
+                    "condor-00041.jpg",
+                    "condor-00042.jpg",
+                    "condor-00043.jpg",
+                    "condor-00044.jpg",
+                    "condor-00045.jpg",
+                    "condor-00046.jpg",
+                    "condor-00047.jpg",
+                    "condor-00048.jpg",
+                    "condor-00049.jpg",
+                    "condor-00050.jpg",
+                    "condor-00051.jpg",
+                    "condor-00052.jpg",
+                    "condor-00053.jpg",
+                    "condor-00054.jpg",
+                    "condor-00055.jpg",
+                    "condor-00056.jpg",
+                    "condor-00057.jpg",
+                    "condor-00058.jpg",
+                    "condor-00059.jpg",
+                    "condor-00060.jpg",
+                    "condor-00061.jpg",
+                    "condor-00062.jpg",
+                    "condor-00063.jpg",
+                    "condor-00064.jpg",
+                    "condor-00065.jpg",
+                    "condor-00066.jpg",
+                    "condor-00068.jpg",
+                    "condor-00069.jpg",
+                    "condor-00070.jpg",
+                    "condor-00071.jpg",
+                    "condor-00072.jpg",
+                    "condor-00073.jpg",
+                    "condor-00074.jpg",
+                    "condor-00075.jpg",
+                    "condor-00076.jpg",
+                    "condor-00077.jpg",
+                    "condor-00078.jpg",
+                    "condor-00079.jpg",
+                    "condor-00080.jpg",
+                    "condor-00081.jpg",
+                    "condor-00082.jpg",
+                    "condor-00083.jpg",
+                    "condor-00084.jpg",
+                    "condor-00085.jpg",
+                    "condor-00086.jpg",
+                    "condor-00087.jpg",
+                    "condor-00088.jpg",
+                    "condor-00089.jpg",
+                    "condor-00090.jpg",
+                    "condor-00091.jpg",
+                    "condor-00092.jpg",
+                    "condor-00093.jpg",
+                    "condor-00094.jpg",
+                    "condor-00095.jpg",
+                    "condor-00096.jpg",
+                    "condor-00097.jpg",
+                    "condor-00098.jpg",
+                    "condor-00099.jpg",
+                    "condor-00100.jpg",
+                    "condor-00101.jpg",
+                    "condor-00102.jpg",
+                    "condor-00103.jpg",
+                    "condor-00104.jpg",
+                    "condor-00105.jpg",
+                    "condor-00106.jpg",
+                    "condor-00107.jpg",
+                    "condor-00108.jpg",
+                    "condor-00109.jpg",
+                    "condor-00110.jpg",
+                    "condor-00111.jpg",
+                    "condor-00112.jpg",
+                    "condor-00113.jpg",
+                    "condor-00114.jpg",
+                    "condor-00115.jpg",
+                    "condor-00116.jpg",
+                    "condor-00117.jpg",
+                    "condor-00118.jpg",
+                    "condor-00119.jpg",
+                    "condor-00120.jpg",
+                    "condor-00121.jpg",
+                    "condor-00122.jpg",
+                    "condor-00123.jpg",
+                    "condor-00124.jpg",
+                    "condor-00125.jpg",
+                    "condor-00126.jpg",
+                    "condor-00127.jpg",
+                    "condor-00128.jpg",
+                    "condor-00129.jpg",
+                    "condor-00130.jpg",
+                    "condor-00131.jpg",
+                    "condor-00132.jpg",
+                    "condor-00133.jpg",
+                    "condor-00134.jpg",
+                    "condor-00135.jpg",
+                    "condor-00136.jpg",
+                    "condor-00137.jpg",
+                    "condor-00138.jpg",
+                    "condor-00139.jpg",
+                    "condor-00140.jpg",
+                    "condor-00141.jpg",
+                    "condor-00142.jpg",
+                    "condor-00143.jpg",
+                    "condor-00144.jpg",
+                    "condor-00145.jpg",
+                    "condor-00146.jpg",
+                    "condor-00147.jpg",
+                    "condor-00148.jpg",
+                    "condor-00149.jpg",
+                    "condor-00150.jpg",
+                    "condor-00151.jpg",
+                    "condor-00152.jpg",
+                    "condor-00153.jpg",
+                    "condor-00154.jpg",
+                    "condor-00155.jpg",
+                    "condor-00156.jpg",
+                    "condor-00157.jpg"
+                    );
+                    foreach ($arr as $value) {
+                        echo "
+                        <div class=\"col-3\">\n
+                            <div class=\"card\" style=\"margin-bottom: 2rem;\">\n
+                                <div class=\"ratio\" style=\"background-image: url(https://marketing.cellar.services.clever-cloud.com/photos/rabe/small/".$value.");\"></div>\n
+                                <p class=\"text-center\" style=\"margin: 0; padding: 0.5rem; font-size:0.7rem;\"><small>".$value."</small></p>
+                                <a class=\"text-center\" href=\"https://marketing.cellar.services.clever-cloud.com/photos/rabe/original/".$value."\" download target=\"_blank\"><small style=\"font-weight: bold; padding-bottom: 0.5rem; display: block;\">Download &darr;</small></a>\n
+                            </div>\n
+                        </div>\n
+                        \n
+                        ";}
+                    ?>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+  </body>
+</html>
