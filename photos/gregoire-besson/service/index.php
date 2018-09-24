8b0f49f0c09d062cf682ed9f061f80703be56eda
Yoann Grange <yoann.grange@gmail.com>
2018-09-24 13:48:03 +0200

    add service photos

---
+++ photos/gregoire-besson/service/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Service Photos - GB Group Marketing Tools &amp; Brand Assets - Grégoire Besson, Rabe, Agriway</title>
+    <meta name="description" content="">
+    <meta name="author" content="GB Group">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext|Kanit:500" rel="stylesheet">
+    <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108104860-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108104860-7');
+    </script>
+
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://marketing.gb-group.co">Back to homepage</a>
+                <br>
+                <br>
+                <h1>Service</h1>
+                <br>
+                <div class="row">
+                    <?php
+                    $arr = array(
+                    "gb-parts-002.jpg",
+                    "gb-parts-003.jpg",
+                    "gb-parts-004.jpg",
+                    "gb-parts-005.jpg",
+                    "gb-parts-006.jpg",
+                    "gb-parts-007.jpg",
+                    "gb-parts-008.jpg",
+                    "gb-parts-009.jpg",
+                    "gb-parts-010.jpg",
+                    "gb-parts-011.jpg",
+                    "gb-parts-012.jpg",
+                    "gb-parts-013.jpg",
+                    "gb-parts-014.jpg",
+                    "gb-parts-015.jpg",
+                    "gb-parts-016.jpg",
+                    "gb-parts-017.jpg",
+                    "gb-parts-018.jpg",
+                    "gb-parts-019.jpg",
+                    "gb-parts-020.jpg",
+                    "gb-parts-021.jpg",
+                    "gb-parts-022.jpg",
+                    "gb-parts-023.jpg",
+                    "gb-parts-024.jpg",
+                    "gb-parts-025.jpg",
+                    "gb-parts-026.jpg",
+                    "gb-parts-027.jpg",
+                    "gb-parts-028.jpg",
+                    "gb-parts-029.jpg",
+                    "gb-parts-030.jpg",
+                    "gb-parts-031.jpg",
+                    "gb-parts-032.jpg",
+                    "gb-parts-033.jpg",
+                    "gb-parts-034.jpg",
+                    "gb-parts-035.jpg",
+                    "gb-parts-036.jpg"
+                    );
+                    foreach ($arr as $value) {
+                        echo "
+                        <div class=\"col-3\">\n
+                            <div class=\"card\" style=\"margin-bottom: 2rem;\">\n
+                                <div class=\"ratio\" style=\"background-image: url(https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/small/".$value.");\"></div>\n
+                                <p class=\"text-center\" style=\"margin: 0; padding: 0.5rem; font-size:0.7rem;\"><small>".$value."</small></p>
+                                <a class=\"text-center\" href=\"https://marketing.cellar.services.clever-cloud.com/photos/gregoire-besson/original/".$value."\" download target=\"_blank\"><small style=\"font-weight: bold; padding-bottom: 0.5rem; display: block;\">Download &darr;</small></a>\n
+                            </div>\n
+                        </div>\n
+                        \n
+                        ";}
+                    ?>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+  </body>
+</html>
